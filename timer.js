47cf6a6d9893c54ffe7814e2e6a8222450887473
g20945tn-1 <105773709+g20945tn-1@users.noreply.github.com>
2022-11-19 03:38:16 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,28 +1,39 @@
-ar PassSec;   // 秒数カウント用変数
- 
-// 繰り返し処理の中身
-function showPassage() {
-   PassSec++;   // カウントアップ
-   var msg = "ボタンを押してから " + PassSec + "秒が経過しました。";   // 表示文作成
-   document.getElementById("PassageArea").innerHTML = msg;   // 表示更新
-}
- 
-// 繰り返し処理の開始
-function startShowing() {
-   PassSec = 0;   // カウンタのリセット
-   PassageID = setInterval('showPassage()',1000);   // タイマーをセット(1000ms間隔)
-   document.getElementById("startcount").disabled = true;   // 開始ボタンの無効化
-}
- 
-// 繰り返し処理の中止
-function stopShowing() {
-   clearInterval( PassageID );   // タイマーのクリア
-   document.getElementById("startcount").disabled = false;   // 開始ボタンの有効化
-}
+const clock = () => {
+  // 現在の日時・時刻の情報を取得
+  const d = new Date();
 
-let button1 = document.getElementById('startcount');
-button1.addEventListener('click', startShowing);
+  // 年を取得
+  let year = d.getFullYear();
+  // 月を取得
+  let month = d.getMonth() + 1;
+  // 日を取得
+  let date = d.getDate();
+  // 曜日を取得
+  let dayNum = d.getDay();
+  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let day = weekday[dayNum];
+  // 時を取得
+  let hour = d.getHours();
+  // 分を取得
+  let min = d.getMinutes();
+  // 秒を取得
+  let sec = d.getSeconds();
 
+  // 1桁の場合は0を足して2桁に
+  month = month < 10 ? "0" + month : month;
+  date = date < 10 ? "0" + date : date;
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
 
-let button2 = document.getElementById('endcount');
-button2.addEventListener('click', stopShowing);
+  // 日付・時刻の文字列を作成
+  let today = `${year}.${month}.${date} ${day}`;
+  let time = `${hour}:${min}:${sec}`;
+
+  // 文字列を出力
+  document.querySelector(".clock-date").innerText = today;
+  document.querySelector(".clock-time").innerText = time;
+};
+
+// 1秒ごとにclock関数を呼び出す
+setInterval(clock, 1000);
